a456e6f573a941270ae81c816df54f607addb7a0
Kanat Kunikeev <divivaes@gmail.com>
2020-10-06 20:08:46 -0400

    player component for entire screen

--- src/components/miniPlayer/miniPlayer.tsx
+++ src/components/miniPlayer/miniPlayer.tsx
@@ -5,60 +5,64 @@ import Icon from 'react-native-vector-icons/Feather';
 import {usePlayerContext} from '../../contexts/PlayerContext';
 import {theme} from '../../constants/theme';
 import {Image, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
 const MiniPlayer = () => {
   const playerContext = usePlayerContext();
+  const navigation = useNavigation();
 
   if (playerContext.isEmpty || !playerContext.currentTrack) {
     return null;
   }
 
   return (
-    <Box
-      h={70}
-      bg="white"
-      px="sm"
-      style={{borderTopWidth: 1, borderTopColor: theme.color.greyLightest}}>
-      <Box f={1} dir="row" align="center" justify="between">
-        <Box
-          h={50}
-          w={50}
-          bg="black"
-          radius={10}
-          mr={10}
-          style={{overflow: 'hidden'}}>
-          <Image
-            source={{uri: playerContext.currentTrack.thumbnail}}
-            style={{flex: 1}}
-          />
-        </Box>
-        <Box f={1} mr={20}>
-          <Text numberOfLines={1}>{playerContext.currentTrack.title}</Text>
-        </Box>
-        <Box>
-          {playerContext.isPaused && (
-            <TouchableOpacity onPress={() => playerContext.play()}>
-              <Icon name="play" size={30} />
+    <TouchableOpacity onPress={() => navigation.navigate('Player')}>
+      <Box
+        h={70}
+        bg="white"
+        px="sm"
+        style={{borderTopWidth: 1, borderTopColor: theme.color.greyLightest}}>
+        <Box f={1} dir="row" align="center" justify="between">
+          <Box
+            h={50}
+            w={50}
+            bg="black"
+            radius={10}
+            mr={10}
+            style={{overflow: 'hidden'}}>
+            <Image
+              source={{uri: playerContext.currentTrack.thumbnail}}
+              style={{flex: 1}}
+            />
+          </Box>
+          <Box f={1} mr={20}>
+            <Text numberOfLines={1}>{playerContext.currentTrack.title}</Text>
+          </Box>
+          <Box mr={10}>
+            {playerContext.isPaused && (
+              <TouchableOpacity onPress={() => playerContext.play()}>
+                <Icon name="play" size={30} />
+              </TouchableOpacity>
+            )}
+            {playerContext.isPlaying && (
+              <TouchableOpacity onPress={playerContext.pause}>
+                <Icon name="pause" size={30} />
+              </TouchableOpacity>
+            )}
+            {playerContext.isStopped && (
+              <TouchableOpacity onPress={() => null}>
+                <Icon name="square" size={30} />
+              </TouchableOpacity>
+            )}
+          </Box>
+          <Box>
+            <TouchableOpacity onPress={() => playerContext.seekTo()}>
+              <Icon size={30} name="rotate-cw" />
             </TouchableOpacity>
-          )}
-          {playerContext.isPlaying && (
-            <TouchableOpacity onPress={playerContext.pause}>
-              <Icon name="pause" size={30} />
-            </TouchableOpacity>
-          )}
-          {playerContext.isStopped && (
-            <TouchableOpacity onPress={() => null}>
-              <Icon name="square" size={30} />
-            </TouchableOpacity>
-          )}
-        </Box>
-        <Box>
-          <TouchableOpacity onPress={() => playerContext.seekTo()}>
-            <Icon size={30} name="rotate-cw" />
-          </TouchableOpacity>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </TouchableOpacity>
   );
 };
 
---
+++ src/components/player/PlayerScreen.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import {Dimensions, Image, StyleSheet} from 'react-native';
+import {Box, Text} from 'react-native-design-utility';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {usePlayerContext} from '../../contexts/PlayerContext';
+import {theme} from '../../constants/theme';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {useNavigation} from '@react-navigation/native';
+
+const {width} = Dimensions.get('window');
+
+const PlayerScreen = () => {
+  const playerContext = usePlayerContext();
+  const navigation = useNavigation();
+
+  const track = playerContext.currentTrack;
+
+  if (!track) {
+    return null;
+  }
+
+  return (
+    <SafeAreaView style={s.safeArea}>
+      <Box f={1} bg="white" pt="md">
+        <Box px="md" mb="md">
+          <TouchableOpacity
+            onPress={() => navigation.goBack}
+            hitSlop={{
+              bottom: 20,
+              top: 20,
+              left: 20,
+              right: 20,
+            }}>
+            <Icon name="chevron-down" size={30} />
+          </TouchableOpacity>
+        </Box>
+        <Box center mb="md">
+          <Image source={{uri: track.artwork}} style={s.img} />
+        </Box>
+        <Box center mb="md" px="md">
+          <Text center bold>
+            {track.title}
+          </Text>
+          <Text color="grey" size="sm">
+            {track.artist}
+          </Text>
+        </Box>
+
+        <Box dir="row" align="center" justify="center">
+          <Box>
+            <TouchableOpacity onPress={() => playerContext.seekTo(-10)}>
+              <Icon size={35} name="rotate-ccw" />
+            </TouchableOpacity>
+          </Box>
+          <Box mx={20}>
+            {playerContext.isPaused ? (
+              <TouchableOpacity onPress={() => playerContext.play()}>
+                <Icon name="play" size={45} />
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity onPress={playerContext.pause}>
+                <Icon name="pause" size={45} />
+              </TouchableOpacity>
+            )}
+          </Box>
+          <Box>
+            <TouchableOpacity onPress={() => playerContext.seekTo()}>
+              <Icon size={35} name="rotate-cw" />
+            </TouchableOpacity>
+          </Box>
+        </Box>
+      </Box>
+    </SafeAreaView>
+  );
+};
+
+const s = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  img: {
+    width: width - theme.space.md * 2,
+    height: width - theme.space.md * 2,
+    borderRadius: 10,
+  },
+});
+
+export default PlayerScreen;
